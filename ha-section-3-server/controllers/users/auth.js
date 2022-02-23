a5d516b49bb7fcc3476ac0ee73a0a43db9eb8cb1
ryurbsgks <ryurbsgks5114@gmail.com>
2022-02-23 20:16:33 +0900

    update ha-section3

--- ha-section-3-server/controllers/users/auth.js
+++ ha-section-3-server/controllers/users/auth.js
@@ -5,5 +5,25 @@ module.exports = (req, res) => {
   const accessTokenData = isAuthorized(req);
   // TODO: 로그인 여부를 판단하고, Access token payload를 이용하여 응답을 제공하세요.
 
-  res.status(401).send({ data: null, message: 'not authorized' });
+  if(!accessTokenData){
+
+    return res.json({data: null, message: "not authorized"})
+  }
+  // id: 1,
+  // email: 'hoyong@codestates.com',
+  // username: 'hoyong',
+  // mobile: '010-1234-5678',
+  // createdAt: '2020-10-10T10:00:12.000Z',
+  // updatedAt: '2020-10-10T10:00:12.000Z',
+
+  let userInfo = {
+    id: accessTokenData.id,
+    email: accessTokenData.email,
+    username: accessTokenData.username,
+    mobile: accessTokenData.mobile,
+    createdAt: accessTokenData.createdAt,
+    updatedAt: accessTokenData.updatedAt
+  }
+
+  res.status(200).send({data: {userInfo: userInfo}})
 };
--- ha-section-3-server/controllers/users/signup.js
+++ ha-section-3-server/controllers/users/signup.js
@@ -1,7 +1,44 @@
 const { user } = require('../../models');
-const { generateAccessToken } = require('../tokenFunctions');
+const { generateAccessToken, sendAccessToken } = require('../tokenFunctions');
 
 module.exports = (req, res) => {
   // TODO: 회원가입 및 사용자 생성 로직을 작성하세요.
-  res.status(500).send();
+
+  if(!req.body.username || !req.body.email || !req.body.password || !req.body.mobile){
+
+    return res.status(422).send("insufficient parameters supplied");
+  }
+
+  user.findOrCreate({
+    where: {
+      email: req.body.email
+    },
+    default: {
+      email: req.body.email,
+      password: req.body.password,
+      username: req.body.username,
+      mobile: req.body.mobile
+    }
+  }).then(([result, created]) =>{
+    if(!created){
+
+      res.status(409).send("email exists");
+    }
+    
+    let data = {
+      id: result.dataValues.id,
+      email: result.dataValues.email,
+      username: result.dataValues.username,
+      mobile: result.dataValues.mobile,
+      createdAt: result.dataValues.createdAt,
+      updatedAt: result.dataValues.updatedAt
+    }
+    const accessToken = generateAccessToken(data);
+
+    sendAccessToken(res, accessToken);
+    res.status(201).send({message: "ok"})
+  }).catch( error => {
+
+    res.status(500).send();
+  })
 };
